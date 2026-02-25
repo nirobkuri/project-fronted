c8d103be713874cf160c1da92822dc2cbd0e0669
Nirob <nirobkuri@gmail.com>
2026-02-25 09:42:54 +0600

    new commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://project-backend-one-sigma.vercel.app/api",
 })
 
 // Attach token to every request if available
